28f6ccfa4f1a399a1b61a31d4343f2654f5bff41
sofy <bedlusofi@gmail.com>
2023-07-13 14:08:45 +0300

    refactor

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,9 @@
 -- a SQL scripts that ranks country origins of bands, ordered by the number of non-uniqque fans
 
-SELECT origin, COUNT(*) AS nb_fans FROM metal_bands
-GROUP BY origin
+SELECT origin, nb_fans
+FROM (
+  SELECT origin, COUNT(DISTINCT id) AS nb_fans
+  FROM metal_bands
+  GROUP BY origin
+) AS subquery
 ORDER BY nb_fans DESC;
