20efb174d560d004f218c4df5bcffb64da6ec3f6
sofy <bedlusofi@gmail.com>
2023-07-13 10:21:25 +0300

    add default option for country column

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXiSTS users(
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255),
 	name VARCHAR(255),
-	country ENUM('US', 'CO', 'TN') NOT NULL 
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
