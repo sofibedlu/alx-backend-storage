cb61907d868b06efb6224467dc26afdeee40b9db
sofy <bedlusofi@gmail.com>
2023-07-13 14:03:05 +0300

    captilize

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- a SQL scripts that ranks country origins of bands, ordered by the number of non-uniqque fans
 
-SELECT origin, COUNT(*) AS nb_fans from metal_bands
+SELECT origin, COUNT(*) AS nb_fans FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
