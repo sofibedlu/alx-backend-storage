3fd2814973c3c7993f9e890c2fc4758a5a5ca67e
sofy <bedlusofi@gmail.com>
2023-07-14 10:41:03 +0300

    update parameter name

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,18 +4,18 @@
 DELIMITER //
 
 CREATE PROCEDURE ComputeAverageScoreForUser(
-	IN user_id INT
+	IN p_user_id INT
 )
 BEGIN
-	DECLARE avg_score FLOAT;
+	DECLARE avg_score DECIMAL(10, 2);
 
 	SELECT AVG(score) INTO avg_score
 	FROM corrections
-	WHERE user_id = user_id;
+	WHERE user_id = p_user_id;
 
 	UPDATE users
 	SET average_score = avg_score
-	WHERE id = user_id;
+	WHERE id = p_user_id;
 END //
 
 DELIMITER //
