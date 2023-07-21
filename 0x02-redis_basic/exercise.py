fa9642ceb0e7f0862393f8d57427aa7645bcd06f
sofy <bedlusofi@gmail.com>
2023-07-21 20:37:32 +0300

    task-1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ define a class Cache
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -25,3 +25,29 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[bytes, str,
+                                                          int, float]:
+        """
+        return the data from the redis with the desired format
+        """
+        data = self._redis.get(key)
+        if data is not None:
+            if fn is not None:
+                return fn(data)
+            return data
+        return None
+
+    def get_str(self, key: str) -> str:
+        """
+        make use of the get method and automatically parametrize it
+        with the correct conversion function
+        """
+        return self.get(key, fn=lambda x: x.decode())
+
+    def get_int(self, key: str) -> int:
+        """
+        make use of the get method and automatically parametrize it
+        with the correct conversion function
+        """
+        return self.get(key, fn=int)
