669651fdf12d6b68d965d5b8531a48a8c5eafbb7
sofy <bedlusofi@gmail.com>
2023-07-22 21:14:32 +0300

    task-9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+define insert_school method
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    inserts a new document in a collection based on kwargs
+    """
+
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
